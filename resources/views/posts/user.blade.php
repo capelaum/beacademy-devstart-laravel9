de16a70909ba556be8d882e5becb80b3cf8abc5e
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-07-11 16:54:38 -0300

    💄 update view

--- resources/views/posts/user.blade.php
+++ resources/views/posts/user.blade.php
@@ -1,37 +1,21 @@
 @extends('template.users')
 @section('title', "{$user->id} | Listagem de Posts")
-@section('heading', 'Listagem de Posts')
+@section('heading', "Listagem de Posts de {$user->name}")
 @section('main')
 
 <h2>Autor</h2>
-<table class="table table-dark table-hover table-striped">
-    <thead class="text-center">
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Avatar</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Email</th>
-        </tr>
-    </thead>
-    <tbody class="text-center">
-        <tr>
-            <td>{{ $user->id }}</td>
-            <td>
-                <img src="{{ $user->image }}" alt="{{ $user->name }}" width="50px" height="50px" class="rounded-circle">
-            </td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-
-        </tr>
-    </tbody>
-</table>
+<div class="mt-4 d-inline-flex flex-column justify-content-center align-items-center border rounded gap-3 p-4">
+    <img src="{{ $user->image }}" alt="{{ $user->name }}" width="50px" height="50px" class="rounded-circle">
+    <span>{{ $user->name }}</span>
+    <span>{{ $user->email }}</span>
+</div>
 
 @if ($posts->isEmpty())
 <p>Não há Posts cadastrados 😕</p>
 
 @else
 
-<h2>Posts</h2>
+<h2 class="mt-5">Posts</h2>
 
 <table class="table table-dark table-hover table-striped">
     <thead class="text-center">
@@ -39,6 +23,7 @@
             <th scope="col">ID</th>
             <th scope="col">Título</th>
             <th scope="col">Conteúdo</th>
+            <th scope="col">Status</th>
             <th scope="col">Data de Criação</th>
         </tr>
     </thead>
@@ -48,7 +33,8 @@
         <tr>
             <td>{{ $post->id }}</td>
             <td>{{ $post->title }}</td>
-            <td style="max-width: 500px">{{ $post->content }}</td>
+            <td style="max-width: 450px">{{ $post->content }}</td>
+            <td>@if($post->active) Ativo @else Inativo @endif</td>
             <td>{{ date('d/m/Y - H:i', strtotime($post->created_at)) }}</td>
         </tr>
 
